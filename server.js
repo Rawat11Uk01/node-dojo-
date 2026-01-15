c8a4f3748f633dbd710b6095891edbb2428b8f44
mskl-kashish <kashish@mentorkool.com>
2026-01-16 03:46:57 +0530

    feat: Implement file reading and server functionality; add HTML template and user input handling

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require("node:http");
+const file = require("fs");
+// creating a server
+const htmlFile = file.readFileSync("./template/index.html", "utf-8");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/secret") {
     return res.end("the secret is you");
   }
-  return res.end(`<h1 style={{border:1px solid red}}>hello world</h1>`);
+  return res.end(htmlFile);
 });
-
+// starting a server
 server.listen(9000);
